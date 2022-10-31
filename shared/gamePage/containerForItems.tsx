82745137913976b9be2c3d5c5fc84111edc87332
Levis <anton_levus@mail.ru>
2022-10-31 11:35:39 +0300

    edited containerForItem.tsx - add blocks for D&D

--- shared/gamePage/containerForItems.tsx
+++ shared/gamePage/containerForItems.tsx
@@ -1,10 +1,60 @@
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 
-export const ContainerForItems = styled.div`
-width: 886px;
-height: 222px;
-background-image: ${({design}) => 
-  `url(${(`/static/${design}/container.png`)})`};
-borderRadius: 50px;
-`
+export const ContainerForItems = ({design}) => {
+  return (
+    <div css={css`
+    width: 886px;
+    height: 222px;
+    background-image: url(/static/${design}/container.png);
+    borderRadius: 50px;
+    display: flex;
+    `}>
+          <div css={css`
+    width: 100px;
+    height: 100px;
+    background-color: blue;
+    margin-top: 100px;
+    margin-left: 100px;
+    `}></div>
+    <div css={css`
+    width: 100px;
+    height: 100px;
+    background-color: green;
+    margin-top: 100px;
+    margin-left: 30px;
+    `}></div>
+    <div css={css`
+    width: 100px;
+    height: 100px;
+    background-color: yellow;
+    margin-top: 100px;
+    margin-left: 40px;
+    `}></div>
+    <div css={css`
+    width: 100px;
+    height: 100px;
+    background-color: black;
+    margin-top: 100px;
+    margin-left: 30px;
+    `}></div>
+    <div css={css`
+    width: 100px;
+    height: 100px;
+    background-color: red;
+    margin-top: 100px;
+    margin-left: 40px;
+    `}></div>
+    <div css={css`
+    width: 100px;
+    height: 100px;
+    background-color: white;
+    margin-top: 100px;
+    margin-left: 30px;
+    `}></div>
+      
+
+    </div>
+    )
+  }
+
